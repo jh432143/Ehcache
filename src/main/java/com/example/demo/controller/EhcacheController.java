d2484d13179e70e2509cdb63e7b5a5c2141bf31a
KJH <jh432143@naver.com>
2019-10-18 13:14:33 +0900

    poi excel 다운로드

--- src/main/java/com/example/demo/controller/EhcacheController.java
+++ src/main/java/com/example/demo/controller/EhcacheController.java
@@ -3,12 +3,17 @@ package com.example.demo.controller;
 import com.example.demo.repository.EhcacheRepository;
 import com.example.demo.service.EhcacheService;
 import com.example.demo.vo.Customer;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.xssf.streaming.SXSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.io.FileOutputStream;
 import java.util.List;
 
 @Controller
@@ -72,4 +77,45 @@ public class EhcacheController {
         return "rootIndex..";
     }
 
+    @RequestMapping(value = "/excelDownTest")
+    public void excelDownTest () {
+        // 새 workbook 생성
+        SXSSFWorkbook wb = new SXSSFWorkbook(100);
+
+        // 새 sheet 생성
+        Sheet sheet = wb.createSheet("First sheet");
+
+        // 새 row 생성
+        Row row = sheet.createRow(0);
+
+        // 새 cell 생성 : title
+        for (int i=0; i<3; i++) {
+            Cell cell = row.createCell(i);
+            cell.setCellValue(i+" title");
+            sheet.setColumnWidth(i, 20*256);
+        }
+
+        // 새 cell 생성 : data
+        Row dataRow = null;
+        List<Customer> list = ehcacheService.findAllByChche();
+        int size = list.size();
+        for (int i=0; i<size; i++) {
+            dataRow = sheet.createRow(i+1);
+
+            dataRow.createCell(0).setCellValue(list.get(i).getId());
+            dataRow.createCell(1).setCellValue(list.get(i).getEmail());
+            dataRow.createCell(2).setCellValue(list.get(i).getName());
+
+        }
+
+        String excelFileName = "C:/kang_work/testExcel.xlsx";
+        try {
+            FileOutputStream out = new FileOutputStream(excelFileName);
+            wb.write(out);
+            out.close();
+            wb.dispose();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
